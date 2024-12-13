cf090df41dbc8bc800eaa82d2c9de4df2b5df31c
Tyler Lewis <tglewis247@gmail.com>
2024-12-13 17:00:02 -0600

    portchange

--- webserver/index.js
+++ webserver/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const sender = require('./sender.js');
-const port = process.env.PORT || 123;
+const port = process.env.PORT || 80;
 var fs = require('fs');
 var server = http.createServer(sender);
 server.listen(port);

d57b7a881ed664a72d41a405b5781d12e2172b4f
Tyler Lewis <tglewis247@gmail.com>
2025-01-20 22:17:37 -0600

    added shut down code

--- webserver/index.js
+++ webserver/index.js
@@ -4,3 +4,12 @@ const port = process.env.PORT || 80;
 var fs = require('fs');
 var server = http.createServer(sender);
 server.listen(port);
+process.on('SIGINT', shutDownServer);
+process.on('SIGTERM', shutDownServer);
+
+function shutDownServer(){
+    console.log("Shutting down server"); 
+    server.close();
+    console.log("Server shut down!\nGoodbye!"); 
+    process.exit(0);
+}
